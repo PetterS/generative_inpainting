8a116aa09ad7afa148f7aae76cfe929041e79a12
Petter Strandmark <petter.strandmark@gmail.com>
2020-07-04 15:47:44 +0200

    Allow many input images at the same time.

--- test.py
+++ test.py
@@ -10,22 +10,14 @@ from inpaint_model import InpaintCAModel
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--image', default='', type=str,
+parser.add_argument('images', nargs="+", type=str,
                     help='The filename of image to be completed.')
 parser.add_argument('--checkpoint_dir', default='', type=str,
                     help='The directory of tensorflow checkpoint.')
 
-def print_info(image, mask):
-    print("Image", image.shape, image.dtype)
-    print("Mask", mask.shape, mask.dtype, set(mask.flatten()))
-    print("")
 
-if __name__ == "__main__":
-    FLAGS = ng.Config('inpaint.yml')
-    # ng.get_gpus(1)
-    args, unknown = parser.parse_known_args()
-
-    image = cv2.imread(args.image, cv2.IMREAD_UNCHANGED)
+def process_image(model, name: str):
+    image = cv2.imread(name, cv2.IMREAD_UNCHANGED)
     alpha = image[:, :, 3].copy()
     image = image[:, :, 0:3].copy()
 
@@ -35,7 +27,7 @@ if __name__ == "__main__":
     mask[bool_mask, 1] = 255
     mask[bool_mask, 2] = 255
 
-    print_info(image, mask)
+    print("Image", image.shape, image.dtype)
 
     assert image.shape == mask.shape
 
@@ -43,22 +35,19 @@ if __name__ == "__main__":
     grid = 8
     image = image[:h//grid*grid, :w//grid*grid, :]
     mask = mask[:h//grid*grid, :w//grid*grid, :]
-    
-    print_info(image, mask)
 
     image = np.expand_dims(image, 0)
     mask = np.expand_dims(mask, 0)
     input_image = np.concatenate([image, mask], axis=2)
 
-    print_info(image, mask)
-
-    model = InpaintCAModel()
+    print("After modification:", image.shape, image.dtype)
 
     sess_config = tf.ConfigProto()
     sess_config.gpu_options.allow_growth = True
     with tf.Session(config=sess_config) as sess:
         input_image = tf.constant(input_image, dtype=tf.float32)
-        output = model.build_server_graph(FLAGS, input_image)
+        output = model.build_server_graph(FLAGS, input_image, reuse=model.petter_reuse)
+        model.petter_reuse = True
         output = (output + 1.) * 127.5
         output = tf.reverse(output, [-1])
         output = tf.saturate_cast(output, tf.uint8)
@@ -73,4 +62,15 @@ if __name__ == "__main__":
         sess.run(assign_ops)
         print('Model loaded.')
         result = sess.run(output)
-        cv2.imwrite(args.image + "_output.png", result[0][:, :, ::-1])
+        cv2.imwrite(name + "_output.png", result[0][:, :, ::-1])
+
+
+if __name__ == "__main__":
+    FLAGS = ng.Config('inpaint.yml')
+    # ng.get_gpus(1)
+    args, unknown = parser.parse_known_args()
+    model = InpaintCAModel()
+    model.petter_reuse = False
+
+    for name in args.images:
+        process_image(model, name)
